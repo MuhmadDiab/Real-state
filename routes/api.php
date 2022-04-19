fe2bf2e4a6392e9451011fc58e735bcd316766f5
MuhmadDiab <muhdiab686@gmail.com>
2022-04-19 10:48:53 +0300

    dsfsdfls

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+
+
 Route::post("register", [CustumarController::class, "register"]);
 Route::post("login", [CustumarController::class, "login"]);
 Route::group(["middleware" => ["auth:sanctum"]], function(){
