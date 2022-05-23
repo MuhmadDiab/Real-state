ed4c7ed0978d610a21786d267a6b0f6002af8122
MuhmadDiab <muhdiab686@gmail.com>
2022-05-23 09:30:37 +0300

    bnvnb

--- app/Http/Controllers/Api/EstateController.php
+++ app/Http/Controllers/Api/EstateController.php
@@ -3,40 +3,58 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Api\BaseController;
+use App\Http\Middleware\CheckRole;
+use App\Http\Requests\EstateRequest;
 use App\Models\Estate;
+use App\Models\User;
+use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Console\MiddlewareMakeCommand;
 use Illuminate\Support\Facades\Validator;
+
 
 
 class EstateController extends BaseController
 {
-
-    public function createEstate(Request $request)
+     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
     {
+        $this->middleware(['auth','checkRole']);
+    }
+
+
+
+    public function createEstate(EstateRequest $request)
+    {
+
+
       // validation
-      $validator = Validator::make($request->all(),[
-        'name'=>'required',
-        'description'=>'required',
-        'roomnumber'=>'required',
-        'state'=>'required',
-        'price'=>'required',
-        'local'=>'required',
-        'lan'=>'required',
-        'lat'=>'required',
-        'photo'=>'required|image',
-        'bathroomnumber'=>'required',
-        'bedroomnumber'=>'required',
-       'propartytype'=>'required',
-      ]);
-        if($validator->fails()){
-        return $this->sendError('Please validate error',$validator->errors);
-       }
+    //   $validator = Validator::make($request->all(),[
+    //     'name'=>'required',
+    //     'description'=>'required',
+    //     'roomnumber'=>'required',
+    //     'state'=>'required',
+    //     'price'=>'required',
+    //     'local'=>'required',
+    //     'lan'=>'required',
+    //     'lat'=>'required',
+    //     'photo'=>'required|image',
+    //     'bathroomnumber'=>'required',
+    //     'bedroomnumber'=>'required',
+    //    'propartytype'=>'required',
+    //   ]);
+    //     if($validator->fails()){
+    //     return $this->sendError('Please validate error',$validator->errors);
+    //    }
 
       $photo=$request->photo;
       $newphoto=time().$photo->getClientOriginalName();
       $photo->move(public_path('upload'),$newphoto);
       $path = "public/upload/$newphoto";
-
       $Estate = new Estate();
       $Estate->name = $request->name;
       $Estate->description = $request->description;
@@ -54,5 +72,40 @@ class EstateController extends BaseController
 
       // send response
       return $this->sendResponse2($Estate,'the Estate created succeflly');
+
+    }
+    public function updateEstate(Request $request ,$id){
+        $validator = Validator::make($request->all(),[
+            'name'=>'required',
+            'description'=>'required',
+            'state'=>'required',
+            'price'=>'required',
+          ]);
+       if($validator->fails())
+        {
+            return $this->sendError('Please validate error',$validator->errors);
+        }
+        $estate =Estate::where('id',$id)->first();
+
+         $estate->update([
+          'name'=>$request->name,
+          'description'=>$request->description,
+          'state'=>$request->state,
+          'price'=>$request->price,
+           ]);
+
+
+           return $this->sendResponse2($estate,'the Estate created succeflly');
+
+      }
+      public function deleteEstate($id)
+    {
+        $estate= Estate::find($id);
+        if(!$estate)
+        {
+            return $this->senderrors('the Estate not found');
+        }
+        $estate->delete();
+        return $this->sendResponse2($estate , 'the Estate deleted succeflly');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CustumarController;
 use App\Http\Controllers\Api\EstateController;
+use App\Http\Controllers\Api\UserestateController;
 
-
-Route::post("register", [CustumarController::class, "register"]);
-Route::post("login", [CustumarController::class, "login"]);
+Route::post("/register", [CustumarController::class, "register"]);
+Route::post("/login", [CustumarController::class, "login"]);
 
 Route::group(["middleware" => ["auth:sanctum"]], function(){
-Route::get("profile", [CustumarController::class, "profile"]);
-Route::post("creatEstate", [EstateController::class, "createEstate"]);
-Route::get("logout", [CustumarController::class, "logout"]);
+Route::get("/profile", [CustumarController::class, "profile"]);
+Route::post("/creatEstate", [EstateController::class, "createEstate"]);
+Route::delete("/deletEstate/{id}", [EstateController::class, "deleteEstate"]);
+Route::get("/showEstate", [UserestateController::class, "listallEstate"]);
+Route::post("/updatEstate/{id}", [EstateController::class, "updateEstate"]);
+Route::get("/logout", [CustumarController::class, "logout"]);
 });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 return $request->user();
